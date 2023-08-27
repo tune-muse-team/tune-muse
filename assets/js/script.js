cde0a3209ad7af5c9f9ad6b1c5f74dbb070b64b0
Shaduruk <jide@tidjani-serpos.com>
2023-08-27 07:11:04 -0400

    feat: get the burger menu to work in mobile display

--- assets/js/script.js
+++ assets/js/script.js
@@ -353,6 +353,36 @@ $(document).on("change", ".chip.chip-checkbox input", function () {
   $chip.attr("aria-checked", this.checked ? "true" : "false");
 });
 
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+   
+  });
+
+  $(".songs-discovered-button").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+ 
+});
+
+
+
+
+
+});
+
+
+
+
+
 goHome();
 
 tuneMuseLogoButton.addEventListener("click", goHome);
